89030ead2071b3baa433683a8c194f23c88f9a88
Shafiul23 <shafiulmirza@hotmail.com>
2024-12-26 14:26:28 +0000

    updated init.py

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -15,22 +14,14 @@ def create_app(test_config=None):
     print("SECRET_KEY is set to:", app.config['SECRET_KEY'])
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
 
     from . import db
     db.init_app(app)
@@ -38,7 +29,6 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    # Enable CORS
     CORS(app, supports_credentials=True)
 
     return app
