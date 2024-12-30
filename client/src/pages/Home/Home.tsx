355937330f7d7a0b560fc73840445b27e0e1ca58
Shafiul23 <shafiulmirza@hotmail.com>
2024-12-30 11:05:26 +0000

    updated home page and made usernames case insensitive

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -13,6 +13,24 @@ const Home: React.FC = () => {
         <p className={styles.subtitle}>
           Test your typing speed and compete with friends!
         </p>
+        <div className={styles.bodyContainer}>
+          <p>
+            Sign in to access the Type Test page where you can choose between a
+            randomized list of 300 common words or a short story.
+          </p>
+          <p>
+            After completing the test, you'll get a score in words per minute
+            (WPM), which is 40 on average.
+          </p>
+          <p>
+            Hit the save button to store your score in the database, which you
+            can view on the profile page.
+          </p>
+          <p>
+            If you manage to get a <strong>top 20 score</strong>, you'll also be
+            able to see it on the leaderboard!
+          </p>
+        </div>
         <div className={styles.buttonGroup}>
           {!userId ? (
             <>
